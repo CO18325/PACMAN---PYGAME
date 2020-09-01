f01868586470c5f9a73c945f2fd3e5289a5cdd97
Inderpreet Singh <inderpreet221099@gmail.com>
2020-09-01 23:08:11 +0530

    COMPLETED THE MAIN SCREEN TEXT DISPLAY REQUIREMENTS INITIATED THE PLAYING ARENA WINDOW COMPANY NAME(CAPIP INNOVATIONS) ADDED TO THE MAIN SCREEN!

--- app_class.py
+++ app_class.py
@@ -20,14 +20,21 @@ class App:
                 self.start_events()
                 self.start_update()
                 self.start_draw()
+            elif self.state == 'playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw() 
+            else:
+                self.running = False
+
             self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
 
 ############################## HELPER FUNCTIONS ##########################
-    def draw_text(self, words, screen, pos, size, color, font_name):
+    def draw_text(self, words, screen, pos, size, color, font_name, centered = False):
         # SET THE FONT AND SIZE
-        font = pygame.font.SysFont(font_name, size, italic=1) 
+        font = pygame.font.SysFont(font_name, size, italic=1, bold=1) 
 
         # FORM THE GIVEN TEXT IN THE FONT WE INITIALIZED
         # ARGS: WORDS TO BE DISPLAYED
@@ -38,11 +45,13 @@ class App:
         # GET THE SIZE OF THE TEXT WRITTEN
         # RETURNS A TUPLE OF (WIDTH, HEIGHT)
         text_size = text.get_size() 
+        if centered:
+            # CENETER THE TEXT TO THE GIVEN POSITION
+            pos[0] = pos[0] - text_size[0]// 2
+            pos[1] = pos[1] - text_size[1]// 2
 
-        # CNETER THE TEXT TO THE GIVEN POSITION
-        pos[0] = pos[0] - text_size[0]// 2
-        pos[1] = pos[1] - text_size[1]// 2
-        screen.blit(text, pos, )
+        # FUNCTION TO DISPLAY ON THE SCREEN 
+        screen.blit(text, pos)
 
 
 ############################## INTRO FUNCTIONS ##########################
@@ -67,8 +76,31 @@ class App:
         self.screen.fill(BLACK)
 
         # HELPER FUNCTION TO DRAW TEXT ON THE PASSED SCREEN
-        # ARGS : TEXT, SCREEN, POSITION OF TEXT, TEXT SIZE, COLOR OF TEXT, FONT OF TEXT
-        self.draw_text('PUSH SPACEBAR', self.screen,[WIDTH//2,HEIGHT//2], START_TEXT_SIZE, ORANGE, START_FONT)
-        
-        
+        # ARGS : TEXT, SCREEN, POSITION OF TEXT, TEXT SIZE, COLOR OF TEXT, FONT OF TEXT, CENTERED 
+        self.draw_text('HIGH SCORE', self.screen,[5,10], START_TEXT_SIZE, WHITE, START_FONT,centered = False)
+
+
+        self.draw_text('PUSH SPACEBAR', self.screen,[WIDTH//2,HEIGHT//2 - 50], START_TEXT_SIZE, ORANGE, START_FONT,centered = True)
+        self.draw_text('1 PLAYER ONLY', self.screen,[WIDTH//2,HEIGHT//2 + 50], START_TEXT_SIZE, WHITE, START_FONT, centered = True)
+        self.draw_text('CAPIP INNOVATIONS', self.screen,[WIDTH//2,HEIGHT//2 + 100], START_TEXT_SIZE, SKYBLUE, START_FONT, centered = True)
+        pygame.display.update()
+
+
+############################## PLAYING FUNCTIONS ##########################
+
+    def playing_events(self):
+        for event in pygame.event.get():
+            # IF EVENT QUIT THEN STOP THE GAME
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    
+    def playing_update(self):
+        pass
+
+    def playing_draw(self):
+
+        # SET SCREEN COLOR TO BLACK
+        self.screen.fill(RED)
+
         pygame.display.update()
